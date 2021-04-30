b9cf0c3dfc8f5f4b6823b76702cb13483c74793d
Federico Reina <federicoreinavinchira@yahoo.com>
2021-04-30 02:27:47 -0500

    removed garbage files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import ImageArray from './components/ImageArray/ImageArray';
 import Letter from './components/Letter/Letter';
 import './App.css';
 
 function App() {
-  const numRows = 10;
-  const numCols = 6;
   return (
     <div className="App">
       <Letter />
